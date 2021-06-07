6e6b15fe7725ffddb958d8c3ba6113e61300e008
sunkyungnoh <cellin1637@gmail.com>
2021-06-08 00:31:36 +0900

    Add : footer 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Nav from './components/Nav.js';
 import Main from './pages/Main/Main.js';
 
 const Routes = () => {
   return (
     <Router>
-      <Header />
+      <Nav />
       <Switch>
         <Route exact path="/" component={Main} />
       </Switch>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <Wrapper>
+      <img alt="logo" src="/images/logo_white.png" />
+      <p>주식회사 와이어즈 | 대표이사 조석현 | 사업자등록변호 691-87-00786</p>
+      <p>
+        서울시 강남구 도산대로 54길 103층 | 통신판매신고업번호
+        제2018-서울마포-1778호 <br /> Copyright 2021 © YerzInc
+      </p>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  height: ${({ theme }) => theme.calcVh(812, 167)};
+  background-color: ${({ theme }) => theme.colors.black};
+
+  & > p {
+    color: white;
+  }
+`;
+
+export default Footer;
