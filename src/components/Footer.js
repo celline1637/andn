d9a1a19e0a9d3c2a4bf25f7f4b402b5ee037d3c9
sunkyungnoh <cellin1637@gmail.com>
2021-06-09 12:00:10 +0900

    Add :  Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+function Footer() {
+  return (
+    <Wrapper>
+      <Logo alt="logo" src="/images/logo_white.png" />
+      <p>주식회사 와이어즈 | 대표이사 조석현 | 사업자등록변호 691-87-00786</p>
+      <p>
+        서울시 강남구 도산대로 54길 103층 | 통신판매신고업번호
+        제2018-서울마포-1778호 <br /> Copyright 2021 © YerzInc
+      </p>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: ${({ theme }) => theme.calcVw(47)} ${({ theme }) => theme.calcVw(21)};
+  background-color: ${({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.calcVw(10)};
+  line-height: ${({ theme }) => theme.calcVw(18)};
+  letter-spacing: ${({ theme }) => theme.calcVw(-0.5)};
+  font-weight: 200;
+  color: white;
+
+  ${({ theme }) => theme.desktop`
+    ${({ theme }) => theme.flexColumnSet()};
+    padding: ${({ theme }) => theme.calcVwL(62)};
+
+  `};
+`;
+
+const Logo = styled.img`
+  width: ${({ theme }) => theme.calcVw(57.9)};
+  height: ${({ theme }) => theme.calcVw(17.7)};
+  margin-bottom: ${({ theme }) => theme.calcVw(14.8)};
+`;
+
+export default Footer;
