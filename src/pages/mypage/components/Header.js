04584eb8d0981d14acd0b9c032fea33730b43bf4
sunkyungnoh <cellin1637@gmail.com>
2021-06-09 16:30:34 +0900

    Add : 마이페이지 헤더

---
+++ src/pages/mypage/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ReactComponent as Arrow } from '../../../assets/arrow.svg';
+import styled from 'styled-components';
+import { Link, useHistory } from 'react-router-dom';
+
+function Header({ text }) {
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <StyledHeader>
+      <ExitArrow width="2.4vw" height="4.8vw" stroke="black" onClick={goBack} />
+      <Text>{text}</Text>
+    </StyledHeader>
+  );
+}
+
+const StyledHeader = styled.header`
+  position: relative;
+  height: ${({ theme }) => theme.calcVw(750, 95)};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+`;
+
+const ExitArrow = styled(Arrow)`
+  ${({ theme }) => theme.posCenterY()};
+  left: ${({ theme }) => theme.calcVw(750, 43)};
+`;
+
+const Text = styled.span`
+  ${({ theme }) => theme.posCenter()};
+  font-size: ${({ theme }) => theme.calcVw(750, 34)};
+  line-height: ${({ theme }) => theme.calcVw(750, 55)};
+  letter-spacing: ${({ theme }) => theme.calcVw(750, -0.85)};
+  font-weight: 500;
+`;
+
+export default Header;
