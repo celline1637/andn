9fa4e7cae8da289f5127cf61cef9c05b0b36d437
sunkyungnoh <cellin1637@gmail.com>
2021-06-17 14:46:11 +0900

    Fix : 컴파일 오류 해결

--- src/pages/main/pay/payment_info/Payment.js
+++ src/pages/main/pay/payment_info/Payment.js
@@ -3,7 +3,7 @@ import Button from '../../../../components/Button';
 import Container from './components/Container';
 import { useRecoilState } from 'recoil';
 import { orderState } from './orderState';
-import styled from 'styled-components/macro/';
+import styled from 'styled-components/macro';
 
 function Payment() {
   const [inputs, setInputs] = useRecoilState(orderState);
