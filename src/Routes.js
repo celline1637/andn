497118d2d7b69d57bacde3aecc26e365b46a4391
sunkyungnoh <cellin1637@gmail.com>
2021-06-21 18:15:35 +0900

    Modify : Route-based code splitting

--- src/Routes.js
+++ src/Routes.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Main from './pages/main/Main';
-import Campaign from './pages/mypage/Campaign';
-import More from './pages/mypage/More';
-import Mypage from './pages/mypage/Mypage';
-import AdminCamp from './pages/admin/AdminCamp';
-import AdminLogin from './pages/admin/AdminLogin';
-import AdminMain from './pages/admin/AdminMain';
-import AdminOrder from './pages/admin/AdminOrder';
-import Change from './pages/change/Change';
-import Password from './pages/change/Password';
-import Pay from './pages/main/pay/Pay';
-import Login from './pages/login/Login';
-import Detail from './pages/main/detail/Detail';
+const Nav = lazy(() => import('./components/Nav'));
+const Home = lazy(() => import('./pages/main/Main'));
+const Pay = lazy(() => import('./pages/main/pay/Pay'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Detail = lazy(() => import('./pages/main/detail/Detail'));
+const Mypage = lazy(() => import('./pages/mypage/Mypage'));
+const Campaign = lazy(() => import('./pages/mypage/Campaign'));
+const More = lazy(() => import('./pages/mypage/More'));
+const AdminCamp = lazy(() => import('./pages/admin/AdminCamp'));
+const AdminLogin = lazy(() => import('./pages/admin/AdminLogin'));
+const AdminMain = lazy(() => import('./pages/admin/AdminMain'));
+const AdminOrder = lazy(() => import('./pages/admin/AdminOrder'));
+const Change = lazy(() => import('./pages/change/Change'));
+const Password = lazy(() => import('./pages/change/Password'));
 
 const Routes = () => {
   return (
     <Router>
       <Nav />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/pay" component={Pay} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/detail/:id" component={Detail} />
-        <Route exact path="/mypage" component={Mypage} />
-        <Route exact path="/mypage_campaign" component={Campaign} />
-        <Route exact path="/mypage_campaign_more/:id" component={More} />
-        <Route exact path="/mypage_change" component={Change} />
-        <Route exact path="/mypage_change_password" component={Password} />
-        <Route exact path="/admin_main" component={AdminMain} />
-        <Route exact path="/admin_login" component={AdminLogin} />
-        <Route exact path="/admin_camp" component={AdminCamp} />
-        <Route exact path="/admin_order/:id" component={AdminOrder} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/pay" component={Pay} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/detail/:id" component={Detail} />
+          <Route exact path="/mypage" component={Mypage} />
+          <Route exact path="/mypage_campaign" component={Campaign} />
+          <Route exact path="/mypage_campaign_more/:id" component={More} />
+          <Route exact path="/mypage_change" component={Change} />
+          <Route exact path="/mypage_change_password" component={Password} />
+          <Route exact path="/admin_main" component={AdminMain} />
+          <Route exact path="/admin_login" component={AdminLogin} />
+          <Route exact path="/admin_camp" component={AdminCamp} />
+          <Route exact path="/admin_order/:id" component={AdminOrder} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
